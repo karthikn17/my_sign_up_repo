49789f5f40e2e624e207a2e85324700dfd6bc45f
Karthik N <karthik-1.n-1@cognizant.com>
2019-04-22 18:42:59 +0800

    HTML form update to app js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const server = http.createServer((req, res) => {
     "headers": { 
 	'Authorization': 'Basic ' + new Buffer.from('systemapiuser:apiuser@123').toString('base64'),
 	"content-type": "application/json"},
-    "url": "https://cognizant-82015.appiancloud.com/suite/webapi/signup/new",
+    "url": "https://cognizant-82015.appiancloud.com/suite/webapi/sign_up",
     "body": JSON.stringify({
     result
   }
@@ -19,24 +19,56 @@ const server = http.createServer((req, res) => {
     if(error) {
         return console.dir(error);
     }
-    console.dir(`Parsed data belonging to ${result.fname}`);
+    console.dir(`Parsed data belonging to ${result.firstName}`);
 });
 
         });
     } 
     else {
         res.end(`
-            <!doctype html>
-            <html>
-            <body>
-                <form action="/" method="post">
-                    <input type="text" name="fname" /><br />
-                    <input type="number" name="age" /><br />
-                    <input type="file" name="photo" /><br />
-                    <button>Save</button>
-                </form>
-            </body>
-            </html>
+            <!DOCTYPE HTML>
+<html>
+   <head>
+         <title>Sign Up</title>
+         <meta charset="utf-8">
+      </head>
+      <body>
+	   <form action="/" method="post">
+	  <h1> Sign Up Form </h1>
+         <h3> Fields marked (*) are mandatory </h3>
+         <fieldset align="center left" style="width:450px;">
+            <legend >Input text field validation</legend>
+            <br>
+            First Name *  <input type="text" name="firstName" placeholder="First Name"><br><br>
+            Last Name *  <input type="text" name="lastName" placeholder="Last Name"><br><br>
+         </fieldset>
+         <br>
+         <fieldset align="center left" style="width:450px;">
+            <legend>Mobile or Phone number</legend>
+            <br>
+            Mobile *  <input type="tel" name="mobile" maxlength="10" size=10 placeholder="Please enter a ten digit phone number"  style="width: 250px;" required><br><br>
+         </fieldset>
+         <br>
+        <br>
+         <fieldset align="center left" style="width:450px;">
+            <legend>Number Validation With Specific Range</legend>
+            <br>
+            Post Code  *   <input type="zip" name="zip" pattern="[0-9]{6}" placeholder="Six digit zip code" title="Six digit zip code" style="width:150px;" required><br><br>
+         </fieldset>
+         <br>
+         <fieldset align="center left" style="width:450px;">
+            <legend>Password </legend>
+            <br>
+            Password * <input type="password" name="pwd" required> <br><br>
+         </fieldset>
+         <br> 
+         <div align = "right" style="width:450px;">
+            <button>Sign Up	</button>
+         </div>
+         <br> 
+		 </form>
+      </body>   
+</html>
         `);
     }
 });
