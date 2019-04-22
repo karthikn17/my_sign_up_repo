d58b3195eeee0a2078430ce6fbbe2b3b622942b2
Karthik N <karthik-1.n-1@cognizant.com>
2019-04-22 16:36:17 +0800

    Added html form for app js

--- app.js
+++ app.js
@@ -7,14 +7,10 @@ var Request = require("request");
 // Running Server Details.
 const http = require('http');
 const port=process.env.PORT || 3000
-const server = http.createServer((req, res) => {
-res.statusCode = 200;
-res.setHeader('Content-Type', 'text/html');
-res.end('<h1>Hello World</h1>');
+app.listen(port, () => {
+    console.log('Server is up and running on port numner ' + port);
 });
-server.listen(port,() => {
-console.log(`Server running at port `+port);
-});
+
  
 app.get('/form', function (req, res) {
   var html='';
