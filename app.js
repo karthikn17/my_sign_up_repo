0fc94b9eca219438a9f451daf0a0b3cab5835877
Karthik N <karthik-1.n-1@cognizant.com>
2019-04-22 16:49:42 +0800

    Updated port and app js details

--- app.js
+++ app.js
@@ -4,66 +4,41 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 var Request = require("request");
  
-// Running Server Details.
+
 const http = require('http');
 const port=process.env.PORT || 3000
-app.listen(port, () => {
-    console.log('Server is up and running on port numner ' + port);
-});
 
+
+//Running Server Details.
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = 3000
+  console.log("Example app listening at %s:%s Port", host, port)
+});
+ 
  
 app.get('/form', function (req, res) {
   var html='';
   html +="<body>";
   html += "<form action='/thank'  method='post' name='form1'>";
-  html += "Name:</p><input type= 'text' name='name'>+";
-  html += "Email:</p><input type='text' name='email'>+";
-  html += "address:</p><input type='text' name='address'>+";
-  html += "Mobile number:</p><input type='text' name='mobilno'>+";
+  html += "Name:</p><input type= 'text' name='name'>";
+  html += "Email:</p><input type='text' name='email'>";
+  html += "address:</p><input type='text' name='address'>";
+  html += "Mobile number:</p><input type='text' name='mobilno'>";
   html += "<input type='submit' value='submit'>";
   html += "<INPUT type='reset'  value='reset'>";
   html += "</form>";
   html += "</body>";
-  res.send(html);  
-
+  res.send(html);
 });
  
 app.post('/thank', urlencodedParser, function (req, res){
-    Request.post({
-    "headers": { 
-	'Authorization': 'Basic ' + new Buffer.from('userId:Pwd').toString('base64'),
-	"content-type": "application/json"},
-    "url": "https://myUrl",
-    "body": JSON.stringify({
-    "nationality": "Ireland",
-    "countryofResidence": "Singapore",
-    "name": "Samwel Tarly",
-    "mobile": "+6598879887",
-    "passportNo": "ABC1234567",
-    "chineseName": "fdf",
-    "gender": "Male",
-    "DOB": "1991-12-27T06:26:43Z",
-    "photo": "CAAS",
-    "passportValidity": "2029-01-30",
-    "email": "raghul.krish@gmail.com",
-    "wechat": "Sam123",
-    "CMOOfficebyregion": "London",
-    "NoOfYrsInEuropeTL": 5,
-    "bankName": "RBS",
-    "bankAccNo" : "122221AAA" ,
-     "requestedBy": "Tour Leader" 
-  }
-)
-}, (error, response, body) => {
-    if(error) {
-        return console.dir(error);
-    }
-    var reply='';
+  var reply='';
   reply += "Your name is" + req.body.name;
   reply += "Your E-mail id is" + req.body.email; 
   reply += "Your address is" + req.body.address;
   reply += "Your mobile number is" + req.body.mobilno;
   res.send(reply);
-  
-});
  });
+
+
