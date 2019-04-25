39341ef3e816a27aefa3a1d6614a0315be691a3c
Karthik N <karthik-1.n-1@cognizant.com>
2019-04-25 13:44:47 +0800

    Updated HTML bg

--- app.js
+++ app.js
@@ -24,6 +24,17 @@ const server = http.createServer((req, res) => {
          <title>Sign Up</title>
          <meta charset="utf-8">
       </head>
+	  <style>
+form {
+  /* Just to center the form on the page */
+  margin: 150px auto;
+  width: 450px;
+  /* To see the outline of the form */
+  padding: 0.5em;  
+  border: 1px solid #282828;
+  border-radius: 1em;
+}
+</style>
       <body>
 	  <h2>Your sign up request has been successfully received<h2> <br><br>
 	  </body>
@@ -40,44 +51,134 @@ const server = http.createServer((req, res) => {
          <title>Sign Up</title>
          <meta charset="utf-8">
 		 <style>
-.content {
-  max-width: 500px;
-  margin: auto;
+form {
+  /* Just to center the form on the page */
+  margin: 150px auto;
+  width: 450px;
+  /* To see the outline of the form */
+  padding: 0.5em;  
+  border: 1px solid #808080;
+  border-radius: 1em;
+}
+
+form div + div {
+  margin-top: 3em;]
+   
+}
+body  {
+  background-size:  contain;
+  background: linear-gradient(to bottom, #33ccff 0%, #ff9900 100%);
+}
+
+
+label {
+  /* To make sure that all labels have the same size and are properly aligned */
+  display: inline-block;
+  width: 90px;
+  text-align: right;
+}
+
+input, textarea {
+
+  padding: 15px;
+  margin: 5px 10px 22px 10px;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input, selection {
+
+  padding: 15px;
+  margin: 5px 10px 22px 10px;
+  display: inline-block;
+  border: none;
+  background: #f1f1f1;
+}
+
+input:focus, textarea:focus {
+  /* To give a little highlight on active elements */
+  border-color: #000;
+}
+
+textarea {
+  /* To properly align multiline text fields with their labels */
+  vertical-align: top;
+
+  /* To give enough room to type some text */
+  height: 5em;
+}
+
+
+.button {
+  /* To position the buttons to the same position of the text fields */
+  padding-left: 90px; /* same size as the label elements */
+}
+
+button {
+  /* This extra margin represent roughly the same space as the space
+     between the labels and their text fields */
+  margin-left: .5em;
+}
+body {
+ 
 }
 </style>
 <body>
-
 <div class="content"> 
   <!-- Page content -->
 </div>
-
 </body>
       </head>
       <body align="center">
 	   <form action="/" method="post">
-	  <h1> Sign Up Form </h1>
-         <fieldset align="left" style="width:450px;">
-            <legend ><h3>Fields marked (*) are mandatory</h3></legend>
-            <br>
-            Name *  <input type="text" name="name" ><br><br>
-            Chinese Name *  <input type="text" name="chineseName"><br><br>
-			Gender *<input type="radio" name="gender" value="Male" checked> Male
-			<input type="radio" name="gender" value="Female"> Female
-			<input type="radio" name="gender" value="Other"> Other<br><br>
-			DOB * <input type="date" name="dob"> <br><br>
-			Mobile * <input type="number" name="mobile"> <br><br>
-			E-mail * <input type="email" name="email"> <br><br>
-			Nationality *  <input type="text" name="nationality" ><br><br>
-			Country of Residence *  <input type="text" name="countryofResidence" ><br><br>
-			We Chat Id  <input type="text" name="wechat"><br><br>
-			Passport No *  <input type="text" name="passportNo"><br><br>
-			Passport Validaty *  <input type="date" name="passportValidity"><br><br>
-			CMO Office by Region *  <input type="text" name="CMOOfficebyregion"><br><br>
-			No of Years in Europe TL *  <input type="number" name="NoOfYrsInEuropeTL"><br><br>
-			Bank Name *  <input type="text" name="bankName"><br><br>
-			Bank Acc No *  <input type="text" name="bankAccNo"><br><br>
-			Requested By *  <input type="text" name="requestedBy"><br><br>
-         </fieldset>
+	 <font color="b36b00"><h1><i> Sign Up Form</i> </h1></font>
+	  <hr>
+           <table> 
+				<tr><td></td><td></td><td></td></tr>
+				<tr> <td>
+ <input type="text" name="name"  placeholder="Name *" required >
+             			</td>
+						<td><td>
+				<td> 
+              <input type="text" name="chineseName"  placeholder="Chinese Name *" required> </td>
+				</tr>
+				<tr> <td> <input type="radio" name="gender" value="male"> Male
+<input type="radio" name="gender" value="female"> Female</td><td><td>
+				<td><input placeholder="DOB *" class="textbox-n" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" name="dob" id="date" required> </td>
+				</tr>
+				<tr> <td><input type="number" name="mobile" placeholder="Mobile *" required> 
+			 </td><td><td>
+				<td> <input type="email" name="email"   placeholder="E-mail *" required>
+			</td>
+				</tr>
+				<tr> <td> <input type="text" name="nationality" placeholder="Nationality *" required >
+			</td><td><td>
+				<td> <input type="text" name="countryofResidence" placeholder="Country of Residence *" required >
+			</td>
+				</tr>
+		.		<tr>
+				<td><input type="text" name="passportNo" placeholder="Passport No *" required>
+					
+			</td><td><td><td><input placeholder="Passport Validaty *" class="textbox-n" type="text" onfocus="(this.type='date')" onblur="(this.type='text')" name="passportValidity" id="passportValidity" required>			
+			</td>
+				</tr>
+				<tr>
+				<td><input type="text" name="wechat" placeholder="We Chat Id *" required>
+			</td><td><td><td><input type="text" name="CMOOfficebyregion" placeholder="CMO Office by Region *" required>
+			</td>
+				</tr>
+				<tr>
+				<td><input type="text" name="bankAccNo" placeholder="Bank Acc No *">
+			</td><td><td><td><input type="text" name="bankName"  placeholder="Bank Name *">
+			</td>
+				</tr>
+				<tr><td><input type="number" name="NoOfYrsInEuropeTL" placeholder="No of Years in Europe TL *"required>
+			</td><td><td>
+				<td><input type="text" name="requestedBy" placeholder="Requested By *">
+        </td></tr>
+				</table>
+                
          <br>  <div align = "right" style="width:450px;">
             <button>Sign Up	</button>
          </div>
